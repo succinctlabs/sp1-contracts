320b206ee01746d94a334f0b6bab656c783f41a0
ratan kaliani <ratan@succinct.xyz>
2024-05-23 12:31:21 -0700

    works

--- src/bin/artifacts.rs
+++ src/bin/artifacts.rs
@@ -1,8 +1,16 @@
 use anyhow::Result;
+use log::info;
 use sp1_sdk::artifacts::try_install_groth16_artifacts;
+use sp1_sdk::utils::setup_logger;
 
 fn main() -> Result<()> {
+    dotenv::dotenv().ok();
+
+    setup_logger();
+
     let artifacts_dir = try_install_groth16_artifacts();
+
+    info!("Artifacts installed to: {:?}", artifacts_dir);
 
     // Write SP1Verifier.sol, ISP1Verifier.sol, SP1MockVerifier.sol and Groth16Verifier.sol from artifacts_dir to ../contracts/src.
     let sp1_verifier_sol = std::fs::read(artifacts_dir.join("SP1Verifier.sol"))?;
@@ -10,7 +18,9 @@ fn main() -> Result<()> {
     let sp1_mock_verifier_sol = std::fs::read(artifacts_dir.join("SP1MockVerifier.sol"))?;
     let groth16_verifier_sol = std::fs::read(artifacts_dir.join("Groth16Verifier.sol"))?;
 
-    let contracts_src_dir = std::path::Path::new("../contracts/src");
+    info!("Writing artifacts to contracts/src");
+
+    let contracts_src_dir = std::path::Path::new("contracts/src");
     std::fs::write(contracts_src_dir.join("SP1Verifier.sol"), sp1_verifier_sol)?;
     std::fs::write(
         contracts_src_dir.join("ISP1Verifier.sol"),
