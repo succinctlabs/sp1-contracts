0ff4f386fd45774859fa7cfbf4f5c245ba2295d2
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-05-22 20:28:48 -0700

    init contracts

---
+++ contracts/src/ISP1Verifier.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+/// @title SP1 Verifier Interface
+/// @author Succinct Labs
+/// @notice This contract is the interface for the SP1 Verifier.
+interface ISP1Verifier {
+    /// @notice Verifies a proof with given public values and vkey.
+    /// @param vkey The verification key for the RISC-V program.
+    /// @param publicValues The public values encoded as bytes.
+    /// @param proofBytes The proof of the program execution the SP1 zkVM encoded as bytes.
+    function verifyProof(
+        bytes32 vkey,
+        bytes memory publicValues,
+        bytes memory proofBytes
+    ) external view;
+}
