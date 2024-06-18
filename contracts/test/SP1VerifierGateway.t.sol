9d7aa3f14da922ca69702649e157c82fe69b5394
mattstam <mattstam@live.com>
2024-06-17 19:04:57 -0700

    tests

--- contracts/test/SP1VerifierGateway.t.sol
+++ contracts/test/SP1VerifierGateway.t.sol
@@ -44,8 +44,8 @@ contract SP1VerifierGatewayTest is Test, ISP1VerifierGatewayEvents, ISP1Verifier
     address internal constant REMOVED_VERIFIER = address(1);
     bytes32 internal constant PROGRAM_VKEY = bytes32(uint256(1));
     bytes internal constant PUBLIC_VALUES = hex"";
-    bytes internal constant PROOF_1 = hex"19ff1d21";
-    bytes internal constant PROOF_2 = hex"fd4b4d23";
+    bytes internal constant PROOF_1 = hex"19ff1d210001";
+    bytes internal constant PROOF_2 = hex"fd4b4d230002";
 
     address internal verifier1;
     address internal verifier2;
@@ -138,94 +138,66 @@ contract SP1VerifierGatewayTest is Test, ISP1VerifierGatewayEvents, ISP1Verifier
         assertEq(frozen, true);
     }
 
-    // function test_RemoveVerifier() public {
-    //     /// Add verifier 1
-    //     bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
-    //     // vm.expectEmit(true, true, true, true);
-    //     // emit RouteUpdated(verifier1Selector, verifier1);
-    //     vm.prank(owner);
-    //     SP1VerifierGateway(gateway).updateVerifier(verifier1Selector, verifier1);
+    function test_RevertFreezeRoute_WhenNotOwner() public {
+        bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                Ownable.OwnableUnauthorizedAccount.selector, makeAddr("notOwner")
+            )
+        );
+        vm.prank(makeAddr("notOwner"));
+        SP1VerifierGateway(gateway).freezeRoute(verifier1Selector);
+    }
 
-    //     assertEq(SP1VerifierGateway(gateway).routes(verifier1Selector), verifier1);
+    function test_RevertFreezeRoute_WhenNoRoute() public {
+        bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
+        vm.expectRevert(abi.encodeWithSelector(RouteNotFound.selector, verifier1Selector));
+        vm.prank(owner);
+        SP1VerifierGateway(gateway).freezeRoute(verifier1Selector);
+    }
 
-    //     /// Remove verifier 1
-    //     vm.expectEmit(true, true, true, true);
-    //     emit RouteUpdated(verifier1Selector, address(0));
-    //     vm.prank(owner);
-    //     SP1VerifierGateway(gateway).updateVerifier(verifier1Selector, address(0));
+    function test_RevertFreezeRoute_WhenRouteIsFrozen() public {
+        bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
+        vm.prank(owner);
+        SP1VerifierGateway(gateway).addRoute(verifier1);
+        vm.prank(owner);
+        SP1VerifierGateway(gateway).freezeRoute(verifier1Selector);
 
-    //     assertEq(SP1VerifierGateway(gateway).routes(verifier1Selector), REMOVED_VERIFIER);
-    // }
+        vm.expectRevert(abi.encodeWithSelector(RouteIsFrozen.selector, verifier1Selector));
+        vm.prank(owner);
+        SP1VerifierGateway(gateway).freezeRoute(verifier1Selector);
+    }
 
-    // function test_RevertUpdateVerifier_WhenNotOwner() public {
-    //     bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
-    //     address notOwner = makeAddr("notOwner");
-    //     vm.expectRevert(
-    //         abi.encodeWithSelector(Ownable.OwnableUnauthorizedAccount.selector, notOwner)
-    //     );
-    //     vm.prank(notOwner);
-    //     SP1VerifierGateway(gateway).updateVerifier(verifier1Selector, verifier1);
-    // }
+    function test_VerifyProof() public {
+        // Add verifier 1
+        vm.prank(owner);
+        SP1VerifierGateway(gateway).addRoute(verifier1);
 
-    // function test_VerifyProof() public {
-    //     // Add verifier 1
-    //     bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
-    //     vm.expectEmit(true, true, true, true);
-    //     emit RouteUpdated(verifier1Selector, verifier1);
-    //     vm.prank(owner);
-    //     SP1VerifierGateway(gateway).updateVerifier(verifier1Selector, verifier1);
+        // Add verifier 2
+        vm.prank(owner);
+        SP1VerifierGateway(gateway).addRoute(verifier2);
 
-    //     // Add verifier 2
-    //     bytes4 verifier2Selector = bytes4(SP1VerifierV2(verifier2).VERIFIER_HASH());
-    //     vm.expectEmit(true, true, true, true);
-    //     emit RouteUpdated(verifier2Selector, verifier2);
-    //     vm.prank(owner);
-    //     SP1VerifierGateway(gateway).updateVerifier(verifier2Selector, verifier2);
+        // Send a proof using verifier 1
+        SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_1);
 
-    //     assertEq(SP1VerifierGateway(gateway).routes(verifier1Selector), verifier1);
-    //     assertEq(SP1VerifierGateway(gateway).routes(verifier2Selector), verifier2);
+        // Send a proof using verifier 2
+        SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_2);
+    }
 
-    //     // Send a proof using verifier 1
-    //     SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_1);
+    function test_RevertVerifyProof_WhenNoRoute() public {
+        bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
+        vm.expectRevert(abi.encodeWithSelector(RouteNotFound.selector, verifier1Selector));
+        SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_1);
+    }
 
-    //     // Send a proof using verifier 2
-    //     SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_2);
-    // }
+    function test_RevertVerifyProof_WhenRouteIsFrozen() public {
+        bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
+        vm.prank(owner);
+        SP1VerifierGateway(gateway).addRoute(verifier1);
+        vm.prank(owner);
+        SP1VerifierGateway(gateway).freezeRoute(verifier1Selector);
 
-    // function test_RevertVerifyProof_WhenRemovedVerifier() public {
-    //     // Add verifier 1
-    //     bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
-    //     vm.expectEmit(true, true, true, true);
-    //     emit RouteUpdated(verifier1Selector, verifier1);
-    //     vm.prank(owner);
-    //     SP1VerifierGateway(gateway).updateVerifier(verifier1Selector, verifier1);
-
-    //     assertEq(SP1VerifierGateway(gateway).routes(verifier1Selector), verifier1);
-
-    //     // Send a proof using verifier 1
-    //     SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_1);
-
-    //     // Remove verifier 1
-    //     vm.expectEmit(true, true, true, true);
-    //     emit RouteUpdated(verifier1Selector, address(0));
-    //     vm.prank(owner);
-    //     SP1VerifierGateway(gateway).updateVerifier(verifier1Selector, address(0));
-
-    //     assertEq(SP1VerifierGateway(gateway).routes(verifier1Selector), REMOVED_VERIFIER);
-
-    //     // Send a proof using verifier 1
-    //     vm.expectRevert(
-    //         abi.encodeWithSelector(SP1VerifierGateway.VerifierRemoved.selector, verifier1Selector)
-    //     );
-    //     SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_1);
-    // }
-
-    // function test_RevertVerifyProof_WhenNotFoundVerifier() public {
-    //     // Send a proof to verifier 1 which was never added
-    //     bytes4 verifier1Selector = bytes4(SP1VerifierV1(verifier1).VERIFIER_HASH());
-    //     vm.expectRevert(
-    //         abi.encodeWithSelector(SP1VerifierGateway.RouteNotFound.selector, verifier1Selector)
-    //     );
-    //     SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_1);
-    // }
+        vm.expectRevert(abi.encodeWithSelector(RouteIsFrozen.selector, verifier1Selector));
+        SP1VerifierGateway(gateway).verifyProof(PROGRAM_VKEY, PUBLIC_VALUES, PROOF_1);
+    }
 }
