16941265e1c5b75551cd81000420f40f7f7b96e9
Ratan Kaliani <ratankaliani@berkeley.edu>
2024-10-16 16:21:47 -0700

    feat: v3.0.0-rc4 + simplify `artifacts` flow + better documentation (#30)
    Co-authored-by: mattstam <codeguy0112358@gmail.com>

--- src/bin/artifacts.rs
+++ src/bin/artifacts.rs
@@ -1,7 +1,7 @@
 use anyhow::Result;
-use log::info;
 use sp1_sdk::install::try_install_circuit_artifacts;
 use sp1_sdk::utils::setup_logger;
+use sp1_sdk::SP1_CIRCUIT_VERSION;
 use std::fs::{create_dir_all, read, read_dir, write};
 use std::path::PathBuf;
 
@@ -15,15 +15,11 @@ fn main() -> Result<()> {
 
     for &artifact_type in &artifact_types {
         let artifacts_dir = try_install_circuit_artifacts(artifact_type);
-        info!(
-            "{} artifacts installed to: {:?}",
-            artifact_type, artifacts_dir
-        );
         artifact_dirs.push(artifacts_dir);
     }
 
     // Read all Solidity files from the artifacts directories.
-    let contracts_src_dir = PathBuf::from("contracts/src");
+    let contracts_src_dir = PathBuf::from(format!("contracts/src/{}", SP1_CIRCUIT_VERSION));
     create_dir_all(&contracts_src_dir)?;
 
     for artifacts_dir in artifact_dirs {
@@ -42,6 +38,59 @@ fn main() -> Result<()> {
             )?;
         }
     }
+
+    println!(
+        "Added the new verifier contracts to {}",
+        contracts_src_dir.display()
+    );
+
+    // Copy deployment scripts from v2.0.0 to {SP1_CIRCUIT_VERSION}
+    let source_deploy_dir = PathBuf::from("contracts/script/deploy/v2.0.0");
+    let target_deploy_dir = PathBuf::from(format!("contracts/script/deploy/{}", SP1_CIRCUIT_VERSION));
+    create_dir_all(&target_deploy_dir)?;
+
+    for entry in read_dir(source_deploy_dir)? {
+        let entry = entry?;
+        let source_path = entry.path();
+        if source_path.is_file() {
+            let file_name = source_path.file_name().unwrap();
+            let target_path = target_deploy_dir.join(file_name);
+            std::fs::copy(&source_path, &target_path)?;
+
+            // Modify SP1VerifierGroth16.s.sol
+            if file_name == "SP1VerifierGroth16.s.sol" {
+                let mut content = String::from_utf8(read(&target_path)?)?;
+                content = content.replace(
+                    "import {SP1Verifier} from \"../../../src/v2.0.0/SP1VerifierGroth16.sol\";",
+                    &format!("import {{SP1Verifier}} from \"../../../src/{}/SP1VerifierGroth16.sol\";", SP1_CIRCUIT_VERSION)
+                );
+                content = content.replace(
+                    "string internal constant KEY = \"V2_0_0_SP1_VERIFIER_GROTH16\";",
+                    &format!("string internal constant KEY = \"{}_SP1_VERIFIER_GROTH16\";", SP1_CIRCUIT_VERSION.replace(".", "_").replace("-", "_").to_uppercase())
+                );
+                write(&target_path, content)?;
+            }
+
+            // Modify SP1VerifierPlonk.s.sol
+            if file_name == "SP1VerifierPlonk.s.sol" {
+                let mut content = String::from_utf8(read(&target_path)?)?;
+                content = content.replace(
+                    "import {SP1Verifier} from \"../../../src/v2.0.0/SP1VerifierPlonk.sol\";",
+                    &format!("import {{SP1Verifier}} from \"../../../src/{}/SP1VerifierPlonk.sol\";", SP1_CIRCUIT_VERSION)
+                );
+                content = content.replace(
+                    "string internal constant KEY = \"V2_0_0_SP1_VERIFIER_PLONK\";",
+                    &format!("string internal constant KEY = \"{}_SP1_VERIFIER_PLONK\";", SP1_CIRCUIT_VERSION.replace(".", "_").replace("-", "_").to_uppercase())
+                );
+                write(&target_path, content)?;
+            }
+        }
+    }
+
+    println!(
+        "Copied and updated deployment scripts to {}",
+        target_deploy_dir.display()
+    );
 
     Ok(())
 }
