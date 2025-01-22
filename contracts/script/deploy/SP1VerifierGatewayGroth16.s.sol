1d8c26aee052c7114ea7df1e66cbff7cd8092e8d
Ratan Kaliani <ratankaliani@berkeley.edu>
2025-01-21 19:56:31 -0800

    freeze route

--- contracts/script/deploy/SP1VerifierGatewayGroth16.s.sol
+++ contracts/script/deploy/SP1VerifierGatewayGroth16.s.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {BaseScript} from "../utils/Base.s.sol";
 import {SP1VerifierGateway} from "../../src/SP1VerifierGateway.sol";
-
+import {ISP1VerifierGateway, VerifierRoute} from "../../src/ISP1VerifierGateway.sol";
 contract SP1VerifierGatewayScript is BaseScript {
     string internal constant KEY = "SP1_VERIFIER_GATEWAY_GROTH16";
 
@@ -23,5 +23,19 @@ contract SP1VerifierGatewayScript is BaseScript {
         // Swap owner
         SP1VerifierGateway gateway = SP1VerifierGateway(0x397A5f7f3dBd538f23DE225B51f532c34448dA9B);
         gateway.transferOwnership(0xBaB2c2aF5b91695e65955DA60d63aD1b2aE81126);
+    }
+
+    function freezeRoute() external multichain(KEY) broadcaster {
+        // Swap owner
+        SP1VerifierGateway gateway = SP1VerifierGateway(0x397A5f7f3dBd538f23DE225B51f532c34448dA9B);
+        bytes4[] memory selectors = new bytes4[](1);
+        selectors[0] = bytes4(0x09069090); // Groth16 V3
+
+        for (uint i = 0; i < selectors.length; i++) {
+            (address verifier, bool frozen) = gateway.routes(selectors[i]);
+            if (!frozen && verifier != address(0)) {
+                gateway.freezeRoute(selectors[i]);
+            }
+        }
     }
 }
--- contracts/script/deploy/SP1VerifierGatewayPlonk.s.sol
+++ contracts/script/deploy/SP1VerifierGatewayPlonk.s.sol
@@ -3,6 +3,9 @@ pragma solidity ^0.8.20;
 
 import {BaseScript} from "../utils/Base.s.sol";
 import {SP1VerifierGateway} from "../../src/SP1VerifierGateway.sol";
+import {ISP1VerifierGateway, VerifierRoute} from "../../src/ISP1VerifierGateway.sol";
+
+import {console} from "forge-std/console.sol";
 
 contract SP1VerifierGatewayScript is BaseScript {
     string internal constant KEY = "SP1_VERIFIER_GATEWAY_PLONK";
@@ -13,7 +16,9 @@ contract SP1VerifierGatewayScript is BaseScript {
         address OWNER = readAddress("OWNER");
 
         // Deploy contract
-        address gateway = address(new SP1VerifierGateway{salt: CREATE2_SALT}(OWNER));
+        address gateway = address(
+            new SP1VerifierGateway{salt: CREATE2_SALT}(OWNER)
+        );
 
         // Write address
         writeAddress(KEY, gateway);
@@ -21,7 +26,42 @@ contract SP1VerifierGatewayScript is BaseScript {
 
     function swapOwner() external multichain(KEY) broadcaster {
         // Swap owner
-        SP1VerifierGateway gateway = SP1VerifierGateway(0x3B6041173B80E77f038f3F2C0f9744f04837185e);
+        SP1VerifierGateway gateway = SP1VerifierGateway(
+            0x3B6041173B80E77f038f3F2C0f9744f04837185e
+        );
         gateway.transferOwnership(0xBaB2c2aF5b91695e65955DA60d63aD1b2aE81126);
+    }
+
+    function freezeRoute() external multichain(KEY) broadcaster {
+        SP1VerifierGateway gateway = SP1VerifierGateway(
+            0x3B6041173B80E77f038f3F2C0f9744f04837185e
+        );
+
+        bytes4[] memory selectors = new bytes4[](18);
+        selectors[1] = bytes4(0xfedc1fcc); // v1.0.1 PLONK
+        selectors[2] = bytes4(0x8c5bc5e4); // 1.0.7-testnet PLONK
+        selectors[3] = bytes4(0x801c66ac); // v1.0.8-testnet PLONK
+        selectors[4] = bytes4(0xc430ff7f); // 1.1.0 PLONK
+        selectors[5] = bytes4(0xc865c1b6); // v1.2.0 PLONK
+        selectors[6] = bytes4(0x837629d3); // v1.2.0 Groth16
+        selectors[7] = bytes4(0x3c65f6c4); // v2.0.0 PLONK
+        selectors[8] = bytes4(0x3c65f6c4); // v2.0.0 PLONK
+        selectors[9] = bytes4(0xb49c53a7); // v3.0.0-rc1 PLONK
+        selectors[10] = bytes4(0x6548aa67); // v3.0.0-rc3 PLONK
+        selectors[11] = bytes4(0x894b4e66); // v3.0.0-rc4 PLONK
+        selectors[12] = bytes4(0x54bdcae3); // PLONK V3
+        selectors[13] = bytes4(0x4aca240a); // PLONK V2
+        selectors[14] = bytes4(0x6a2906ac); // Groth16 V2
+        selectors[15] = bytes4(0x3e923f1f); // v3.0.0-rc1 Groth16
+        selectors[16] = bytes4(0x2a12d1d0); // v3.0.0-rc4 Groth16
+        selectors[17] = bytes4(0x09069090); // v3.0.0 Groth16
+
+        for (uint i = 0; i < selectors.length; i++) {
+            (address verifier, bool frozen) = gateway.routes(selectors[i]);
+            if (!frozen && verifier != address(0)) {
+                console.logBytes4(selectors[i]);
+                gateway.freezeRoute(selectors[i]);
+            }
+        }
     }
 }
