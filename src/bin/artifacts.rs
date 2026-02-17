db686f904c4bb804d400c719ca327dbfd896aa44
yuwen <yuwen@succinct.xyz>
2026-02-17 07:14:51 -0800

    merge marker

--- src/bin/artifacts.rs
+++ src/bin/artifacts.rs
@@ -15,11 +15,7 @@ async fn main() -> Result<()> {
     let mut artifact_dirs = Vec::new();
 
     for &artifact_type in &artifact_types {
-<<<<<<< HEAD
         let artifacts_dir = try_install_circuit_artifacts(artifact_type).await?;
-=======
-        let artifacts_dir = try_install_circuit_artifacts(artifact_type).await;
->>>>>>> yuwen/v6
         artifact_dirs.push(artifacts_dir);
     }
 
